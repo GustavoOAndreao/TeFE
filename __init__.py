7b486161cf886af8810382ca6391b945f7cb81e6
GustavoOAndreao <gustavo.93.andreao@gmail.com>
2021-10-25 22:06:06 -0300

    Create LICENSE.md git config core.autocrlf true

--- __init__.py
+++ __init__.py
@@ -4,11 +4,12 @@
 #                                                               #
 #################################################################
 
+# check before importing
 
-import random    
+import random
 import feather
 import simpy
-#!pip install simpy #on colab it must be this pip install thing, dunno why
+# !pip install simpy #on colab it must be this pip install thing, dunno why
 import numpy as np
 import pandas as pd
 import math as math
@@ -17,7 +18,7 @@ import networkx as nx
 import time
 from matplotlib import pyplot as plt
 import itertools
-import uuid 
+import uuid
 import pdb
 import statistics
 from icecream import ic
